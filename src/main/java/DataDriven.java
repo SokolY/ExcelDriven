1a156419bb6a3a8d75e88289a61bb64aa1a86df7
sokoly <yura.sokol@gmail.com>
2022-05-25 22:57:50 +0300

    Access to the row

--- src/main/java/DataDriven.java
+++ src/main/java/DataDriven.java
@@ -1,7 +1,10 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Iterator;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -9,17 +12,29 @@ public class DataDriven {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		XSSFSheet sheet = null;
 		FileInputStream is = new FileInputStream("D:\\FromPC\\DiscD\\Java\\ExcelDataDriven\\ExcelDriven\\src\\main\\resources\\TestData.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(is);
 		
 		int sheets = workbook.getNumberOfSheets();
 		System.out.println("Number of sheets is: " +sheets);
 		for(int i = 0; i<sheets; i++) {
-//			System.out.println("Sheet name is: " + workbook.getSheetName(i));
-			if(workbook.getSheetName(i).equalsIgnoreCase("Profile"){
-				XSSFSheet sheet = workbook.getSheetAt(i);
-			}
+			System.out.println("Sheet name is: " + workbook.getSheetName(i));
+			if(workbook.getSheetName(i).equalsIgnoreCase("Profile")){
+				sheet = workbook.getSheetAt(i);
+			}			
 		}
+		Iterator<Row> rows = sheet.iterator();
+		rows.next();
+		rows.next();
+		Row firstRow = rows.next();
+		Iterator<Cell> cells =  firstRow.cellIterator();
+		while(cells.hasNext()) {
+			Cell value = cells.next();
+			System.out.println(value.getStringCellValue());
+
+		}
+		
 	}
 
 }
